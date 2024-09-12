fb7fd0829e57e1c3e0090c85436d01c56b635ac5
ParkJongMin <jmpark@underwriter.co.kr>
2024-09-12 19:46:21 +0900

    feat - 환경변수 추가 세팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
+    API_KEY: process.env.API_KEY,
+  },
   images: {
     remotePatterns: [
       {
