7e5332165f69f9e3cc2bcec87552e23f372ae2c0
ParkJongMin <jmpark@underwriter.co.kr>
2024-09-12 19:34:32 +0900

    feat - 환경변수 테스트

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Category from "./components/Category";
 import PagePadding from "@/components/PagePadding";
@@ -10,9 +11,17 @@ const page = async () => {
   const dummyPlaylistArray2 = [await getPlaylistById(1)];
   const dummyPlaylistArray3 = [await getPlaylistById(2)];
   const dummyPlaylistArray4 = [await getPlaylistById(3)];
+  const env = process.env;
+  const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT;
+  const apiKey = process.env.API_KEY;
+
+  console.log(`process.env: ${env}`);
+  console.log(`process.env.NEXT_PUBLIC_API_ENDPOINT: ${apiEndpoint}`);
+  console.log(`process.env.API_KEY: ${apiKey}`);
 
   return (
     <PagePadding>
+      <div></div>
       <div className="min-h-[600px]">
         <div className="mt-9"></div>
         <Category />
