33aa4a48f576d33ec89c2838abcbc99f2dddb69d
ParkJongMin <jmpark@underwriter.co.kr>
2024-09-12 16:14:41 +0900

    feat - PlayListCarousel 및 타이핑

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,13 +1,31 @@
 import { sleep } from "@/lib/utils";
 import React from "react";
 import Category from "./components/Category";
+import PagePadding from "@/components/PagePadding";
+import PlayListCarousel from "@/components/PlayListCarousel";
+import { dummyPlaylistArray } from "@/lib/dummyData";
+import UserIcon from "@/components/UserIcon";
 
 const page = async () => {
+  const dummyPlaylistArray1 = [...dummyPlaylistArray];
   return (
-    <div className="min-h-[600px]">
-      <div className="mt-9">HomePage</div>
-      <Category />
-    </div>
+    <PagePadding>
+      <div className="min-h-[600px]">
+        <div className="mt-9">HomePage</div>
+        <Category />
+        <div className="mt-12"></div>
+        <PlayListCarousel
+          playlistArray={[...dummyPlaylistArray1]}
+          Thumnail={
+            <div className="h-[56px] w-[56px]">
+              <UserIcon />
+            </div>
+          }
+          title="다시듣기"
+          subTitle="도도"
+        />
+      </div>
+    </PagePadding>
   );
 };
 
